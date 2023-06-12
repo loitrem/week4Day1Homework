1a2efa035bef2a91bf6cf956b5c1c7bc0679a93e
loitrem <ski_fix@hotmail.com>
2023-06-12 15:29:34 -0400

    init commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// How do we assign a value to a variable?
+let a = 1;
+
+// How do we change the value of a variable?
+a = 2;
+
+// How do we assign an existing variable to a new variable?
+let b = a;
+
+// Remind me, what are declare, assign, and define?
+// declare is using let to assign something as a variable, assign is using = to assign that variable a value, defining is once its been declared in the current scope
+
+// What is pseudocoding and why should you do it?
+// pseudocoding is using comments to describe what the code should be doing
+
+// What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+// 
