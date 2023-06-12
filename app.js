4bccd710c3fbf9932763a7c73342580432b5b946
loitrem <ski_fix@hotmail.com>
2023-06-12 16:13:41 -0400

    done with 2-d

--- app.js
+++ app.js
@@ -148,3 +148,22 @@ console.log(48 !== '48');
         }
             
     }
+
+// Write code that will save the sum of all the numbers between 1 - 10 to a variable called bank_account.
+let bank_account = 0;
+for (i=1;i<=10;i++){
+    bank_account += i;
+    
+}
+
+// Check your work! Your bank_account should have $55 in it.
+console.log("bank account total is $" + bank_account);
+
+// You got a bonus! Your pay is now doubled each week. Write code that will save the sum of all the numbers between 1 - 100 multiplied by 2.
+    bank_account = 0;
+    for (i=1;i<=100;i++){
+        bank_account += (i*2);
+        
+    }
+// Check your work! Your bank_account should have $10,100 in it.
+console.log("bank account total is $" + bank_account);
